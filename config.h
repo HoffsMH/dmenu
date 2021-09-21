05e02536e04b9c1f7842ebacaa07aadaeafb641f
HoffsMH <matthecker@pm.me>
2021-09-20 22:48:07 -0500

    ok

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't us
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-  "IBM Plex Sans Cond SmBld:size=25"
+  "IBM Plex Sans Cond SmBld:size=20"
 };
 
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
@@ -20,7 +20,7 @@ static const char *colors[SchemeLast][2] = {
 static unsigned int lines      = 0;
 
 /* -h option; minimum height of a menu line */
-static unsigned int lineheight = 100;
+static unsigned int lineheight = 25;
 static unsigned int min_lineheight = 100;
 
 /*
